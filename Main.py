de3dc335aae574101a122d4aa9aee8e6c48df44b
akshayalix <akshayalix12@gmail.com>
2022-07-22 14:21:23 +0530

    - added main_py

---
+++ Main.py
@@ -0,0 +1,11 @@
+# Cleaning Script for saving time.
+# It will automaticaly clean all the necessary things like, temp, prefetch, etc.
+
+# Import dependencies --
+
+import pyautogui
+# from pynput.keyboard import key, Controller #
+import os
+import time
+
+# keyboard = Controller()  # Define keyboard Controller.
