4732a089d8da5af0fa21aa5f76bbbe319322b432
akshayalix <akshayalix12@gmail.com>
2023-02-09 21:09:29 +0530

    added prefetch

--- Clear.bat
+++ Clear.bat
@@ -1,7 +1,31 @@
 @ECHO OFF
 
+:: Clear %temp% 
+
 cd /D %temp%
 
 for /d %%D in (*) do rd /s /q "%%D"
 
 del /f /q *
+
+::................
+
+:: Clear temp
+
+cd /D C:\Windows\Temp\
+
+for /d %%D in (*) do rd /s /q "%%D"
+
+del /f /q *
+
+::..................
+
+:: Clear Prefetch
+
+cd /D C:\Windows\Prefetch\
+
+for /d %%D in (*) do rd /s /q "%%D"
+
+del /f /q *
+
+::.......................
