bfed3d30dd36789dcc42434ac1f703decfcbb191
Alix <akshayalix12@gmail.com>
2024-07-28 13:22:28 +0530

    Update Optimize_all.bat

--- Optimize_all.bat
+++ Optimize_all.bat
@@ -1,8 +1,18 @@
 @echo off
+:: Check for admin privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell.exe -Command "Start-Process cmd -ArgumentList '/c %~fnx0' -Verb RunAs"
+    exit
+)
+
 set drives=C D E
 
 for %%i in (%drives%) do (
     echo Optimizing drive %%i:
     defrag %%i: /O /V
 )
+
+
 
